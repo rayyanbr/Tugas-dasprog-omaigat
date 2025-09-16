eed9baa2676046b8192379318dcfd940ac6f2037
rayyanbr <rayyanbr123@gmail.com>
2025-09-16 15:54:39 +0700

    db fix

--- week 3/db.py
+++ week 3/db.py
@@ -2,18 +2,26 @@ import math
 
 n = int(input())
 numbers = []
+results = []
+
 for i in range(n):
     q = int(input())
+    numbers.append(q)
+
+for q in numbers:
     lim = math.isqrt(q)
     found = False
-    for a in range(lim + 1):
-        for b in range(lim + 1):
-            for c in range(lim + 1):
-                for d in range(lim + 1):
+    for a in range(1, lim + 1):
+        for b in range(1, lim + 1):
+            for c in range(1, lim + 1):
+                for d in range(1, lim + 1):
                     if a*a + b*b + c*c + d*d == q:
                         found = True
                         break
                 if found: break
             if found: break
         if found: break
-    print("LEAVE" if found else "ERASE")
+    results.append("LEAVE" if found else "ERASE")
+for res in results:
+    print(res)
+    
